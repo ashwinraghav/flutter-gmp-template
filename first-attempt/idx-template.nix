1ffe0f116719260a388ee65ba59e77c8f2bd00da
Ashwin Raghav <ashwinraghav@google.com>
2024-04-03 14:40:15 -0700

    Update idx-template.nix

--- first-attempt/idx-template.nix
+++ first-attempt/idx-template.nix
@@ -5,9 +5,7 @@
     ];
   bootstrap = ''
     mkdir -p "$WS_NAME"
-    mkdir -p "$WS_NAME/repo/"
     git clone --depth 1 ${giturl} repo
-    cp -r repo "$WS_NAME/repo"
     echo ${apikey} > "$WS_NAME/apikey.txt"
     chmod -R +w "$WS_NAME"
     mkdir -p "$WS_NAME/.idx/"
