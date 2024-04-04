4ea32639231005e8635d1a81ff8ca0c5b626f35d
Ashwin Raghav <ashwinraghav@google.com>
2024-04-03 17:06:57 -0700

    Update dev.nix

--- first-attempt/dev.nix
+++ first-attempt/dev.nix
@@ -1,1 +1,29 @@
+# To learn more about how to use Nix to configure your environment
+# see: https://developers.google.com/idx/guides/customize-idx-env
+{ pkgs, ... }: {
+  # Which nixpkgs channel to use.
+  channel = "stable-23.11"; # or "unstable"
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    pkgs.nodePackages.firebase-tools
+    pkgs.jdk17
+    pkgs.sdkmanager
+    pkgs.android-tools
+  ];
+  # Sets environment variables in the workspace
+  env = {};
+  idx = {
 
+    previews = {
+      enable = true;
+      previews = [
+        {
+          # command = ["./.flutter-sdk/flutter/bin/flutter" "run" "--machine" "-d" "android" "-d" "localhost:5555"];
+          command = ["./gradlew :app:assembleDebug && adb install ./app/build/outputs/apk/debug/app-debug.apk"];
+          id = "android";
+          manager = "flutter";
+        }
+      ];
+    };
+  };
+}
