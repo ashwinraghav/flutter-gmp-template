317d1a2148f264d0e8053250a9adf136302fb5d5
Ashwin Raghav <ashwinraghav@google.com>
2024-04-11 17:19:15 -0700

    Update idx-template.nix

--- flutter-attempt/idx-template.nix
+++ flutter-attempt/idx-template.nix
@@ -1,1 +1,11 @@
-
+{ pkgs, apikey ? "missingkey", giturl ? "missingurl", subdir ? "", launchactivity ? "",... }: {
+  packages = [
+      pkgs.git
+      pkgs.sdkmanager
+      pkgs.j2cli
+  ];
+  bootstrap = ''
+    flutter create "$WS_NAME"
+    mv "$WS_NAME" "$out"
+  '';
+}
